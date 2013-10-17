107e641db12004726d6e830356fb77819d442311
OlgaE <olga.efimova.aol@gmail.com>
2013-10-17 14:15:29 +0700

    Using Vaadin with Maven and Spring. Working)

---
+++ VaadinApp1/src/main/java/com/demo/VaadinApp1/MyBeanInterface.java
@@ -0,0 +1,9 @@
+package com.demo.VaadinApp1;
+
+public class MyBeanInterface {
+
+	public String myMethod() {
+		
+		return "Hello from the bean :)";
+	}
+}
---
+++ VaadinApp1/src/main/java/com/demo/VaadinApp1/SpringContextHelper.java
@@ -0,0 +1,20 @@
+package com.demo.VaadinApp1;
+
+import javax.servlet.ServletContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+public class SpringContextHelper {
+
+    private ApplicationContext context;
+    
+    public SpringContextHelper(ServletContext servletContext) {
+
+    	context = WebApplicationContextUtils.
+                getRequiredWebApplicationContext(servletContext);
+    }
+
+    public Object getBean(final String beanRef) {
+        return context.getBean(beanRef);
+    }    
+}
